e3dba4cd13624490b0bdc6a4c1ca30f62837c2c0
Bruce Pike <bruce.pike@openstax.org>
2017-08-03 17:44:44 -0500

    Added Deploy.py to create deploy.zip and install.

---
+++ JiraInteraction/Deploy.py
@@ -0,0 +1,33 @@
+# MSP Update deploy
+# Creates deploy.zip and installs to MyDocuments\bin\MSP Update
+
+import shutil
+import os
+import zipfile
+
+SrcDrctry = "C:\\Users\\Bruce Pike Rice\\Documents\\Repos\\work-management\\"
+Trgt = SrcDrctry + "Deploy\\"
+InstllDrctry = "C:\\Users\\Bruce Pike Rice\\Documents\\bin\\MSP Update\\"
+
+# Copy release folder contents
+RlsFldr = SrcDrctry + "JiraInteraction\\bin\\Release\\"
+FlLst = os.listdir(RlsFldr)
+for Itm in FlLst:
+    #print ("item = %s" %(RlsFldr + Itm))
+    shutil.copy(RlsFldr + Itm, Trgt)
+
+# Copy other files
+shutil.copy(SrcDrctry + "JiraInteraction\\UTS MSP Update Template.xlsm", Trgt)
+
+shutil.copy(SrcDrctry + "JiraInteraction\\UTS MSP Update Readme.txt", Trgt)
+
+# Create deploy.zip
+shutil.make_archive(SrcDrctry + "deploy", 'zip', Trgt)
+
+# Copy deploy zip to install directory and unzip
+shutil.copy(SrcDrctry + "deploy.zip", InstllDrctry)
+zip_ref = zipfile.ZipFile(InstllDrctry + "deploy.zip", 'r')
+zip_ref.extractall(InstllDrctry + "Deploy\\")
+zip_ref.close()
+
+wait = input("All done - PRESS ENTER TO CONTINUE.")
